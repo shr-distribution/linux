3549a68b3aa1b4edd2ca963d1d43d9ccaf20a402
Sakari Ailus <sakari.ailus@nokia.com>
2008-10-28 11:54:04 +0200

    et8ek8: Toshiba 5MP sensor driver
    Squashed from the following upstream commits:
    
    et8ek8: Add driver
    et8ek8: Expect sensor not to be streaming for s_fmt
    et8ek8: cosmetics
    et8ek8: window stuff
    et8ek8: improve V4L2 control handling
    et8ek8: change exposure units from rows to microseconds
    et8ek8: replace g_smia_mode with V4L2 controls
    et8ek8: Rework initialisation
    et8ek8: sensor i2c interface must be initialized before reading EEPROM
    et8ek8: Power up fixes
    et8ek8: add support for V4L2_CID_TEST_PATTERN
    et8ek8: suspend/resume support for et8ek8
    et8ek8: configure interface now takes struct smia_mode as parameter
    et8ek8: Fix suspend
    et8ek8: Get rid of vidioc_int_enum_slaves
    et8ek8: update V4L2 control (ie. exposure) limits
    et8ek8: enable streaming after sensor configuration
    et8ek8: update to new power management model
    et8ek8: at power on, first enable clocks and after that release reset
    et8ek8: decrease delays
    et8ek8: FIRMWARE_NAME_MAX -> SMIA_MAX_LEN
    et8ek8: Remove unused header file containing register lists
    et8ek8: Convert to v4l2_subdev model
    et8ek8: Implement s_stream
    et8ek8: Kill unused variable in et8ek8
    et8ek8: Add a media entity output pad
    et8ek8: Use entity power ops
    et8ek8: media entity cleanup memory leak.
    et8ek8: Kill unused timeperframe field in struct et8ek8_sensor.
    et8ek8: Pad operations
    et8ek8: Get rid of regular format operations
    et8ek8: Replace video::g/s_parm with video::g/s_frame_interval
    et8ek8: Add media bus pixel code enumeration
    et8ek8: Update the get_fmt/set_fmt operations to the new API
    et8ek8: Update to the new pad::enum_mbus_code operation
    et8ek8: Implement subdev pad-level frame size enumeration
    et8ek8: Implement subdev pad-level frame interval enumeration
    et8ek8: Include linux/slab.h.
    et8ek8: Add V4L2_CID_MODE_OPSYSCLOCK for reading output system clock
    et8ek8: Update to the latest smiaregs API
    et8ek8: Update to the latest v4l2_subdev API
    
    Signed-off-by: Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: David Cohen <david.cohen@nokia.com>

---
+++ drivers/media/video/et8ek8.h
@@ -0,0 +1,79 @@
+/*
+ * drivers/media/video/et8ek8.h
+ *
+ * Copyright (C) 2008 Nokia Corporation
+ *
+ * Contact: Sakari Ailus <sakari.ailus@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef ET8EK8_H
+#define ET8EK8_H
+
+#include <linux/i2c.h>
+#include <media/smiaregs.h>
+#include <media/v4l2-subdev.h>
+#include <media/media-entity.h>
+
+#define ET8EK8_NAME		"et8ek8"
+#define ET8EK8_I2C_ADDR		(0x7C >> 1)
+
+#define ET8EK8_PRIV_MEM_SIZE	128
+#define ET8EK8_NCTRLS		3
+
+struct et8ek8_platform_data {
+	int (*g_priv)(struct v4l2_subdev *subdev, void *priv);
+	int (*configure_interface)(struct v4l2_subdev *subdev,
+				   struct smia_mode *mode);
+	int (*set_xclk)(struct v4l2_subdev *subdev, int hz);
+	int (*s_power)(struct v4l2_subdev *subdev, int on);
+};
+
+struct et8ek8_sensor;
+
+/* Current values for V4L2 controls */
+struct et8ek8_control {
+	s32 minimum;
+	s32 maximum;
+	s32 step;
+	s32 default_value;
+	s32 value;
+	int (*set)(struct et8ek8_sensor *sensor, s32 value);
+};
+
+#define to_et8ek8_sensor(sd)	container_of(sd, struct et8ek8_sensor, subdev)
+
+struct et8ek8_sensor {
+	struct v4l2_subdev subdev;
+	struct media_pad pad;
+	struct v4l2_mbus_framefmt format;
+	struct et8ek8_platform_data *platform_data;
+
+	u16 version;
+
+	struct et8ek8_control controls[ET8EK8_NCTRLS];
+
+	struct smia_reglist *current_reglist;
+
+	const struct firmware *fw;
+	struct smia_meta_reglist *meta_reglist;
+	u8 priv_mem[ET8EK8_PRIV_MEM_SIZE];
+
+	int power : 1;
+};
+
+#endif /* ET8EK8_H */
