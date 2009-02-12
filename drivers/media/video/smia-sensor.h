32e154616e6742468b0a995a17416ca63ca45b86
Tuukka.O Toivonen <tuukka.o.toivonen@nokia.com>
2009-02-12 12:50:37 +0200

    smia-sensor: Generic SMIA sensor driver
    Squashed from the following upstream commits:
    
    smia-sensor: add driver
    smia-sensor: suspend/resume support
    smia-sensor: add table of known sensors
    smia-sensor: Fix suspend
    smia-sensor: Get rid of vidioc_int_enum_slaves
    smia-sensor: update to new power management model
    smia-sensor: FIRMWARE_NAME_MAX -> SMIA_MAX_LEN
    smia-sensor: Convert to v4l2_subdev model
    smia-sensor: Implement s_stream
    smia-sensor: Kill unused variable in smia-sensor
    smia-sensor: cosmetics: move struct smia_sensor from header into .c file
    smia-sensor: add pointer from struct smia_sensor to sensor type structure
    smia-sensor: clean up V4L2 controls
    smia-sensor: add dynamic frame format detection for the sensor
    smia-sensor: read analog gain limits from the sensor directly
    smia-sensor: split update_control and set_control functionality
    smia-sensor: fix writing gain value to the sensor.
    smia-sensor: fix indentation
    smia-sensor: fix sparse warnings
    smia-sensor: release firmware on device removal
    smia-sensor: Add a media entity output pad
    smia-sensor: Use entity power ops
    smia-sensor: media entity cleanup memory leak
    smia-sensor: drop setting sensor to stream-off before configure
    smia-sensor: fix sensor configuration
    smia-sensor: Add V4L2 subdev pad operations
    smia-sensor: Remove V4L2 subdev video {get|try|set}_format operations
    Revert "smia-sensor: Add V4L2 subdev pad operations"
    smia-sensor: Add V4L2 subdev pad operations
    smia-sensor: Fix fencepost error addressing sensor array
    smia-sensor: Implement the subdev sensor::g_skip_top_lines operation
    smia-sensor: Replace video::g/s_parm with video::g/s_frame_interval
    smia-sensor: Add media bus pixel code enumeration
    smia-sensor: Update the get_fmt/set_fmt operations to the new API
    smia-sensor: Implement subdev pad-level frame size enumeration
    smia-sensor: Update to the new pad::enum_mbus_code operation API
    smia-sensor: Implement subdev pad-level frame interval enumeration
    smia-sensor: Include linux/slab.h.
    smia-sensor: Update to the latest smiaregs API
    smia-sensor: Update to the latest v4l2_subdev API
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Vimarsh Zutshi <vimarsh.zutshi@nokia.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
    Signed-off-by: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
    Signed-off-by: David Cohen <david.cohen@nokia.com>
    Signed-off-by: Phil Carmody <ext-phil.2.carmody@nokia.com>
    Signed-off-by: Stanimir Varbanov <svarbanov@mm-sol.com>

---
+++ drivers/media/video/smia-sensor.h
@@ -0,0 +1,39 @@
+/*
+ * drivers/media/video/smia-sensor.h
+ *
+ * Copyright (C) 2008,2009 Nokia Corporation
+ *
+ * Contact: Tuukka Toivonen <tuukka.o.toivonen@nokia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ */
+
+#ifndef SMIA_SENSOR_H
+#define SMIA_SENSOR_H
+
+#include <media/v4l2-subdev.h>
+
+#define SMIA_SENSOR_NAME	"smia-sensor"
+#define SMIA_SENSOR_I2C_ADDR	(0x20 >> 1)
+
+struct smia_sensor_platform_data {
+	int (*configure_interface)(struct v4l2_subdev *subdev,
+				   int sof_rows, int height);
+	int (*set_xclk)(struct v4l2_subdev *subdev, int hz);
+	int (*set_power)(struct v4l2_subdev *subdev, int on);
+};
+
+#endif /* SMIA_SENSOR_H */
