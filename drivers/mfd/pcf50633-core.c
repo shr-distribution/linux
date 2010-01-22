c098430eebd42df4f956326aa7654f6e25a3b5d9
Lars-Peter Clausen <lars@metafoo.de>
2010-01-22 12:47:01 +0100

    pcf50633-gpio: Add gpiolib support.
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- drivers/mfd/pcf50633-core.c
+++ drivers/mfd/pcf50633-core.c
@@ -152,7 +152,7 @@ static struct attribute_group pcf_attr_group = {
 
 static void
 pcf50633_client_dev_register(struct pcf50633 *pcf, const char *name,
-						struct platform_device **pdev)
+							struct platform_device **pdev)
 {
 	int ret;
 
@@ -253,7 +253,8 @@ static int __devinit pcf50633_probe(struct i2c_client *client,
 						&pcf->adc_pdev);
 	pcf50633_client_dev_register(pcf, "pcf50633-backlight",
 						&pcf->bl_pdev);
-
+	pcf50633_client_dev_register(pcf, "pcf50633-gpio",
+						&pcf->gpio_pdev);
 
 	for (i = 0; i < PCF50633_NUM_REGULATORS; i++) {
 		struct platform_device *pdev;
@@ -297,6 +298,7 @@ static int __devexit pcf50633_remove(struct i2c_client *client)
 	sysfs_remove_group(&client->dev.kobj, &pcf_attr_group);
 	pcf50633_irq_free(pcf);
 
+	platform_device_unregister(pcf->gpio_pdev);
 	platform_device_unregister(pcf->input_pdev);
 	platform_device_unregister(pcf->rtc_pdev);
 	platform_device_unregister(pcf->mbc_pdev);
