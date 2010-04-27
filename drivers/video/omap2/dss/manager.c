a0004305543bddb7534bbf70b89e7eba43a21909
Tomi Valkeinen <tomi.valkeinen@nokia.com>
2010-04-27 17:34:02 +0300

    OMAP: DSS2: in_use flag for dss_cache
    This patch introduces in_use flag for manager_cache_data. The flag can
    be enabled with omap_dss_lock_cache() and disabled with
    omap_dss_unlock_cache().
    
    When the flag is enabled, manager->apply doesn't change the cache for
    that manager. When the flag is disabled with omap_dss_unlock_cache(),
    the settings will be applied.
    
    This was not required previously as all updates were done with one
    transfer to the panel, and the cache values were written to hardware
    registers at the start of the transfer. Thus there was no need to lock
    the cache.
    
    When the update is done in two parts, the two parts have to be
    transferred with the same DSS settings. This means that we need to lock
    the cache for the duration of the transfer so that nobody will change
    it.
    
    [ext-kimmo.jukarainen@nokia.com: updated to apply in 3.0]
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@nokia.com>
    Signed-off-by: Kimmo Jukarainen <ext-kimmo.jukarainen@nokia.com>

--- drivers/video/omap2/dss/manager.c
+++ drivers/video/omap2/dss/manager.c
@@ -447,7 +447,11 @@ struct manager_cache_data {
 	/* enlarge the update area if the update area contains scaled
 	 * overlays */
 	bool enlarge_update_area;
+
+	bool in_use;
 };
+
+static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr);
 
 static struct {
 	spinlock_t lock;
@@ -457,7 +461,26 @@ static struct {
 	bool irq_enabled;
 } dss_cache;
 
+void omap_dss_lock_cache(void)
+{
+	unsigned long flags;
+	spin_lock_irqsave(&dss_cache.lock, flags);
+	BUG_ON(dss_cache.manager_cache[0].in_use);
+	dss_cache.manager_cache[0].in_use = true;
+	spin_unlock_irqrestore(&dss_cache.lock, flags);
+}
+EXPORT_SYMBOL(omap_dss_lock_cache);
 
+void omap_dss_unlock_cache(void)
+{
+	unsigned long flags;
+	spin_lock_irqsave(&dss_cache.lock, flags);
+	BUG_ON(!dss_cache.manager_cache[0].in_use);
+	dss_cache.manager_cache[0].in_use = false;
+	spin_unlock_irqrestore(&dss_cache.lock, flags);
+	omap_dss_mgr_apply(omap_dss_get_overlay_manager(0));
+}
+EXPORT_SYMBOL(omap_dss_unlock_cache);
 
 static int omap_dss_set_device(struct omap_overlay_manager *mgr,
 		struct omap_dss_device *dssdev)
@@ -1248,6 +1271,12 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 			continue;
 
 		oc = &dss_cache.overlay_cache[ovl->id];
+		if (ovl->manager) {
+			mc = &dss_cache.manager_cache[ovl->manager->id];
+
+			if (mc->in_use)
+				continue;
+		}
 
 		if (!overlay_enabled(ovl)) {
 			if (oc->enabled) {
@@ -1319,6 +1348,9 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 
 		mc = &dss_cache.manager_cache[mgr->id];
 
+		if (mc->in_use)
+			continue;
+
 		if (mgr->device_changed) {
 			mgr->device_changed = false;
 			mgr->info_dirty  = true;
@@ -1376,6 +1408,13 @@ static int omap_dss_mgr_apply(struct omap_overlay_manager *mgr)
 			continue;
 
 		oc = &dss_cache.overlay_cache[ovl->id];
+
+		if (ovl->manager) {
+			mc = &dss_cache.manager_cache[ovl->manager->id];
+
+			if (mc->in_use)
+				continue;
+		}
 
 		if (!oc->enabled)
 			continue;
