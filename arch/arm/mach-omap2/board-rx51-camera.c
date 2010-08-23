2c551ee8885b0f6e97023f912876c9f20e4d4543
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2010-08-23 11:38:35 +0200

    rx51 cam: Drop power control from board file
    This patch is in preparation to move to the regulator framework. We
    should no more access the TWL5030 registers directly, so drop all power
    handling from board files.
    
    The handling of the VANA signal for the camera should be done by the
    camera sensor and lens driver. And the handling of the VDDA_VCSI signal
    should be done by the ISP driver.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- arch/arm/mach-omap2/board-rx51-camera.c
+++ arch/arm/mach-omap2/board-rx51-camera.c
@@ -51,240 +51,6 @@
 #define STINGRAY_RESET_GPIO	102
 #define ACMELITE_RESET_GPIO	97	/* Used also to MUX between cameras */
 
-#define RX51_CAMERA_STINGRAY	0
-#define RX51_CAMERA_ACMELITE	1
-
-#define RX51_SENSOR		1
-#define RX51_LENS		2
-
-#define GPIO_DIR_OUTPUT		0
-
-/*
- *
- * Power control
- *
- */
-
-/* Assign camera to peripheral power group P3 */
-#define CAMERA_DEV_GRP		(0x4 << 5)
-#define VAUX2_1V8		0x05
-#define VAUX3_1V8		0x01
-#define VAUX4_2V8		0x09
-
-/* Earlier rx51 builds require VAUX3. */
-#define NEEDS_VAUX3		(system_rev >= 0x100 && system_rev < 0x900)
-
-static struct rx51_camera {
-	int okay;
-	int inuse;
-} rx51_camera[2];
-
-static DEFINE_MUTEX(rx51_camera_mutex);
-
-/* Acquires the given slave `which' for camera if possible.
- * Returns the bitmask containing previously acquired slaves for the device.
- */
-static int rx51_camera_acquire_slave(int camera, int which)
-{
-	int other = 1 - camera;
-	int old_which;
-
-	if (!rx51_camera[camera].okay)
-		return -EINVAL;
-
-	if (rx51_camera[other].inuse)
-		return -EBUSY;
-
-	old_which = rx51_camera[camera].inuse;
-	rx51_camera[camera].inuse |= which;
-
-	return old_which;
-}
-
-/* Releases the given slave `which' for camera.
- * Returns the bitmask containing still acquired slaves for the device.
- */
-static int rx51_camera_release_slave(int camera, int which)
-{
-	rx51_camera[camera].inuse &= ~which;
-
-	return rx51_camera[camera].inuse;
-}
-
-static int rx51_camera_power_on_nolock(int camera)
-{
-	int rval;
-
-	/* Reset Stingray */
-	gpio_set_value(STINGRAY_RESET_GPIO, 0);
-
-	/* Mux to Stingray and reset Acme Lite */
-	gpio_set_value(ACMELITE_RESET_GPIO, 0);
-
-	/* VAUX2=1.8 V (muxer voltage) */
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    VAUX2_1V8, TWL4030_VAUX2_DEDICATED);
-	if (rval)
-		goto out;
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    CAMERA_DEV_GRP, TWL4030_VAUX2_DEV_GRP);
-	if (rval)
-		goto out;
-
-	/* Off & sleep -> Active state */
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    0xEE, TWL4030_VAUX2_REMAP);
-	if (rval)
-		goto out;
-
-	/* VAUX4=2.8 V (camera VANA) */
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    VAUX4_2V8, TWL4030_VAUX4_DEDICATED);
-	if (rval)
-		goto out;
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    CAMERA_DEV_GRP, TWL4030_VAUX4_DEV_GRP);
-	if (rval)
-		goto out;
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    0xEE, TWL4030_VAUX4_REMAP);
-	if (rval)
-		goto out;
-
-	if (NEEDS_VAUX3) {
-		/* VAUX3=1.8 V (camera VDIG) */
-		printk(KERN_INFO "%s: VAUX3 on for old board\n", __func__);
-		rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-					    VAUX3_1V8,
-					    TWL4030_VAUX3_DEDICATED);
-		if (rval)
-			goto out;
-		rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-					    CAMERA_DEV_GRP,
-					    TWL4030_VAUX3_DEV_GRP);
-		if (rval)
-			goto out;
-		rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-					    0xEE, TWL4030_VAUX3_REMAP);
-		if (rval)
-			goto out;
-	}
-
-	/* Let the voltages stabilize */
-	udelay(15);
-
-	/* XSHUTDOWN on, enable camera and set muxer */
-	gpio_set_value(camera == RX51_CAMERA_STINGRAY ?
-		       STINGRAY_RESET_GPIO : ACMELITE_RESET_GPIO, 1);
-
-	/* CONTROL_CSIRXFE */
-	omap_writel(
-		/*
-		 * CSIb receiver data/clock or data/strobe mode
-		 *
-		 * Stingray uses data/strobe.
-		 */
-		((camera ? 0 : 1) << 10)
-		| BIT(12)       /* Enable differential transceiver */
-		| BIT(13)       /* Disable reset */
-		, OMAP343X_CTRL_BASE + OMAP343X_CONTROL_CSIRXFE);
-
-	/* Let the voltages stabilize */
-	udelay(15);
-
-	return 0;
-
-out:
-	printk(KERN_ALERT "%s: Error %d in writing to TWL4030!\n", __func__,
-	       rval);
-
-	return rval;
-}
-
-static int rx51_camera_power_on(int camera, int which)
-{
-	int rval;
-
-	mutex_lock(&rx51_camera_mutex);
-
-	rval = rx51_camera_acquire_slave(camera, which);
-
-	if (!rval)
-		rval = rx51_camera_power_on_nolock(camera);
-	else if (rval > 0)
-		rval = 0;
-
-	mutex_unlock(&rx51_camera_mutex);
-
-	if (rval < 0)
-		printk(KERN_INFO "%s: power_on camera %d which %d failed\n",
-		       __func__, camera, which);
-
-	return rval;
-}
-
-static void rx51_camera_power_off_nolock(int camera)
-{
-	int rval;
-
-	/* Reset cameras */
-	gpio_set_value(STINGRAY_RESET_GPIO, 0);
-	gpio_set_value(ACMELITE_RESET_GPIO, 0);
-
-	/* VAUX2 (muxer voltage) off */
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    0, TWL4030_VAUX2_DEV_GRP);
-	if (rval)
-		goto out;
-	/* Off & sleep -> Off state */
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    0x00, TWL4030_VAUX2_REMAP);
-	if (rval)
-		goto out;
-
-	/* VAUX4 (camera VANA) off */
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    0, TWL4030_VAUX4_DEV_GRP);
-	if (rval)
-		goto out;
-	rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-				    0x00, TWL4030_VAUX4_REMAP);
-	if (rval)
-		goto out;
-
-	if (NEEDS_VAUX3) {
-		printk(KERN_INFO "%s: VAUX3 off for old board\n", __func__);
-		/* VAUX3 (camera VDIG) off */
-		rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-					    0, TWL4030_VAUX3_DEV_GRP);
-		if (rval)
-			goto out;
-		rval = twl_i2c_write_u8(TWL4030_MODULE_PM_RECEIVER,
-					    0x00, TWL4030_VAUX3_REMAP);
-		if (rval)
-			goto out;
-	}
-
-	return;
-
-out:
-	printk(KERN_ALERT "%s: Error %d in writing to TWL4030!\n", __func__,
-	       rval);
-}
-
-static void rx51_camera_power_off(int camera, int which)
-{
-	int rval;
-
-	mutex_lock(&rx51_camera_mutex);
-
-	rval = rx51_camera_release_slave(camera, which);
-	if (!rval)
-		rx51_camera_power_off_nolock(camera);
-
-	mutex_unlock(&rx51_camera_mutex);
-}
-
 static void __init rx51_stingray_init(void)
 {
 	if (gpio_request(STINGRAY_RESET_GPIO, "stingray reset") != 0) {
@@ -295,9 +61,7 @@ static void __init rx51_stingray_init(void)
 
 	/* XSHUTDOWN off, reset  */
 	gpio_direction_output(STINGRAY_RESET_GPIO, 0);
-	rx51_camera_power_off_nolock(RX51_CAMERA_STINGRAY);
-	rx51_camera[RX51_CAMERA_STINGRAY].okay = 1;
-	rx51_camera[RX51_CAMERA_STINGRAY].inuse = 0;
+	gpio_set_value(STINGRAY_RESET_GPIO, 0);
 }
 
 static void __init rx51_acmelite_init(void)
@@ -310,9 +74,7 @@ static void __init rx51_acmelite_init(void)
 
 	/* XSHUTDOWN off, reset  */
 	gpio_direction_output(ACMELITE_RESET_GPIO, 0);
-	rx51_camera_power_off_nolock(RX51_CAMERA_ACMELITE);
-	rx51_camera[RX51_CAMERA_ACMELITE].okay = 1;
-	rx51_camera[RX51_CAMERA_ACMELITE].inuse = 0;
+	gpio_set_value(ACMELITE_RESET_GPIO, 0);
 }
 
 static int __init rx51_adp1653_init(void)
@@ -366,7 +128,6 @@ static int __init rx51_camera_hw_init(void)
 	if (rval)
 		return rval;
 
-	mutex_init(&rx51_camera_mutex);
 	rx51_stingray_init();
 	rx51_acmelite_init();
 
@@ -416,20 +177,9 @@ static int rx51_stingray_set_xclk(struct v4l2_subdev *subdev, int hz)
 	return 0;
 }
 
-static int rx51_stingray_s_power(struct v4l2_subdev *subdev, int on)
-{
-	if (on)
-		return rx51_camera_power_on(RX51_CAMERA_STINGRAY, RX51_SENSOR);
-	else
-		rx51_camera_power_off(RX51_CAMERA_STINGRAY, RX51_SENSOR);
-
-	return 0;
-}
-
 static struct et8ek8_platform_data rx51_et8ek8_platform_data = {
 	.configure_interface	= rx51_stingray_configure_interface,
 	.set_xclk		= rx51_stingray_set_xclk,
-	.s_power		= rx51_stingray_s_power,
 };
 
 /*
@@ -438,19 +188,7 @@ static struct et8ek8_platform_data rx51_et8ek8_platform_data = {
  *
  */
 
-static int ad5820_s_power(struct v4l2_subdev *subdev, int on)
-{
-	if (on)
-		return rx51_camera_power_on(RX51_CAMERA_STINGRAY, RX51_LENS);
-	else
-		rx51_camera_power_off(RX51_CAMERA_STINGRAY, RX51_LENS);
-
-	return 0;
-}
-
 static struct ad5820_platform_data rx51_ad5820_platform_data = {
-	.s_power	= ad5820_s_power,
-
 };
 
 /*
@@ -509,20 +247,9 @@ static int rx51_acmelite_set_xclk(struct v4l2_subdev *subdev, int hz)
 	return 0;
 }
 
-static int rx51_acmelite_set_power(struct v4l2_subdev *subdev, int on)
-{
-	if (on)
-		return rx51_camera_power_on(RX51_CAMERA_ACMELITE, RX51_SENSOR);
-	else
-		rx51_camera_power_off(RX51_CAMERA_ACMELITE, RX51_SENSOR);
-
-	return 0;
-}
-
 static struct smia_sensor_platform_data rx51_smia_sensor_platform_data = {
 	.configure_interface	= rx51_acmelite_configure_interface,
 	.set_xclk		= rx51_acmelite_set_xclk,
-	.set_power		= rx51_acmelite_set_power,
 };
 
 /*
