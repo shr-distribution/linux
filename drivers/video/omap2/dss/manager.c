23036a1a7abf68115dda67b010416cfa58360c43
Pauli Nieminen <ext-pauli.nieminen@nokia.com>
2010-08-25 18:47:09 +0300

    OMAP: DSS2: return error values down to caller
    ->notify() can return error that wasn't returned back to caller.
    
    Reported-by: Ville Syrjälä <ville.syrjala@nokia.com>
    Signed-off-by: Pauli Nieminen <ext-pauli.nieminen@nokia.com>

--- drivers/video/omap2/dss/manager.c
+++ drivers/video/omap2/dss/manager.c
@@ -1180,6 +1180,7 @@ int dss_mgr_notify_ovl(struct omap_overlay *ovl)
 int omap_dss_request_notify(enum omap_dss_notify_event event,
 			    long value)
 {
+	int r;
 	struct omap_overlay_manager *mgr;
 	struct omap_overlay *ovl;
 
@@ -1190,7 +1191,9 @@ int omap_dss_request_notify(enum omap_dss_notify_event event,
 			return -EINVAL;
 		if (!mgr->notify)
 			return -ENOSYS;
-		mgr->notify(mgr);
+		r = mgr->notify(mgr);
+		if (r)
+			return r;
 		break;
 	case OMAP_DSS_NOTIFY_GO_OVL:
 		ovl = omap_dss_get_overlay(value);
@@ -1198,7 +1201,9 @@ int omap_dss_request_notify(enum omap_dss_notify_event event,
 			return -EINVAL;
 		if (!ovl->notify)
 			return -ENOSYS;
-		ovl->notify(ovl);
+		r = ovl->notify(ovl);
+		if (r)
+			return r;
 		break;
 	default:
 		return -EINVAL;
