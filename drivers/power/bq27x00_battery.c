95259648a27094dae2bf6e024bc20c18f2ccb59b
Denis 'GNUtoo' Carikli <GNUtoo@no-log.org>
2010-11-01 20:31:35 +0100

    power: bq27x00_battery.c: import registers from the openmoko driver
    The openmoko driver register are much more detailed than the one from
      the i2c bq27x00

--- drivers/power/bq27x00_battery.c
+++ drivers/power/bq27x00_battery.c
@@ -27,23 +27,107 @@
 #include <linux/slab.h>
 #include <asm/unaligned.h>
 
+
+#if 0
 #define DRIVER_VERSION			"1.1.0"
 
-#define BQ27x00_REG_TEMP		0x06
-#define BQ27x00_REG_VOLT		0x08
-#define BQ27x00_REG_AI			0x14
-#define BQ27x00_REG_FLAGS		0x0A
-#define BQ27x00_REG_TTE			0x16
-#define BQ27x00_REG_TTF			0x18
-#define BQ27x00_REG_TTECP		0x26
+#define BQ27x00_REG_TEMP		0x06 //   BQ27x00_TEMP_L OK
+#define BQ27x00_REG_VOLT		0x08 //   BQ27x00_VOLT_L OK
+#define BQ27x00_REG_AI			0x14 //   BQ27x00_AI_L OK
+#define BQ27x00_REG_FLAGS		0x0A //   BQ27x00_FLAGS OK
+#define BQ27x00_REG_TTE			0x16 //   BQ27x00_TTE_L OK
+#define BQ27x00_REG_TTF			0x18 //   BQ27x00_TTF_L OK
+#define BQ27x00_REG_TTECP		0x26 //   BQ27x00_TTECP_L OK
 
-#define BQ27000_REG_RSOC		0x0B /* Relative State-of-Charge */
-#define BQ27000_FLAG_CHGS		BIT(7)
+#define BQ27000_REG_RSOC		0x0B //   BQ27x00_RSOC OK
+#define BQ27000_FLAG_CHGS		BIT(7) // BQ27000_STATUS_CHGS 
 
-#define BQ27500_REG_SOC			0x2c
-#define BQ27500_FLAG_DSC		BIT(0)
+#define BQ27500_REG_SOC			0x2c //   BQ27x00_CSOC OK
+#define BQ27500_FLAG_DSC		BIT(0) 
 #define BQ27500_FLAG_FC			BIT(9)
+#else
 
+#define DRIVER_VERSION			"1.1.1"
+/*imported from old driver */
+#define BQ27500_FLAG_DSC		BIT(0) /* Discharging flag */
+#define BQ27500_FLAG_FC			BIT(9) /* Full capacity */
+
+enum bq27x00_regs {
+	/* RAM regs */
+		/* read-write after this */
+	BQ27x00_CTRL = 0, /* Device Control Register */
+	BQ27x00_MODE, /* Device Mode Register */
+	BQ27x00_AR_L, /* At-Rate H L */
+	BQ27x00_AR_H,
+		/* read-only after this */
+	BQ27x00_ARTTE_L, /* At-Rate Time To Empty H L */
+	BQ27x00_ARTTE_H,
+	BQ27x00_TEMP_L, /* Reported Temperature H L */ 
+	BQ27x00_TEMP_H,
+	BQ27x00_VOLT_L, /* Reported Voltage H L */
+	BQ27x00_VOLT_H,
+	BQ27x00_FLAGS, /* Status Flags */
+	BQ27x00_RSOC, /* Relative State of Charge */
+	BQ27x00_NAC_L, /* Nominal Available Capacity H L */
+	BQ27x00_NAC_H,
+	BQ27x00_CACD_L, /* Discharge Compensated H L */
+	BQ27x00_CACD_H,
+	BQ27x00_CACT_L, /* Temperature Compensated H L */
+	BQ27x00_CACT_H,
+	BQ27x00_LMD_L, /* Last measured discharge H L */
+	BQ27x00_LMD_H,
+	BQ27x00_AI_L, /* Average Current H L */
+	BQ27x00_AI_H,
+	BQ27x00_TTE_L, /* Time to Empty H L */
+	BQ27x00_TTE_H,
+	BQ27x00_TTF_L, /* Time to Full H L */
+	BQ27x00_TTF_H,
+	BQ27x00_SI_L, /* Standby Current H L */
+	BQ27x00_SI_H,
+	BQ27x00_STTE_L, /* Standby Time To Empty H L */
+	BQ27x00_STTE_H,
+	BQ27x00_MLI_L, /* Max Load Current H L */
+	BQ27x00_MLI_H,
+	BQ27x00_MLTTE_L, /* Max Load Time To Empty H L */
+	BQ27x00_MLTTE_H,
+	BQ27x00_SAE_L, /* Available Energy H L */
+	BQ27x00_SAE_H,
+	BQ27x00_AP_L, /* Available Power H L */
+	BQ27x00_AP_H,
+	BQ27x00_TTECP_L, /* Time to Empty at Constant Power H L */
+	BQ27x00_TTECP_H,
+	BQ27x00_CYCL_L, /* Cycle count since learning cycle H L */
+	BQ27x00_CYCL_H,
+	BQ27x00_CYCT_L, /* Cycle Count Total H L */
+	BQ27x00_CYCT_H,
+	BQ27x00_CSOC, /* Compensated State Of Charge */
+	/* EEPROM regs */
+		/* read-write after this */
+	BQ27x00_EE_EE_EN = 0x6e, /* EEPROM Program Enable */
+	BQ27x00_EE_ILMD = 0x76, /* Initial Last Measured Discharge High Byte */
+	BQ27x00_EE_SEDVF, /* Scaled EDVF Threshold */
+	BQ27x00_EE_SEDV1, /* Scaled EDV1 Threshold */
+	BQ27x00_EE_ISLC, /* Initial Standby Load Current */
+	BQ27x00_EE_DMFSD, /* Digital Magnitude Filter and Self Discharge */
+	BQ27x00_EE_TAPER, /* Aging Estimate Enable, Charge Termination Taper */
+	BQ27x00_EE_PKCFG, /* Pack Configuration Values */
+	BQ27x00_EE_IMLC, /* Initial Max Load Current or ID #3 */
+	BQ27x00_EE_DCOMP, /* Discharge rate compensation constants or ID #2 */
+	BQ27x00_EE_TCOMP, /* Temperature Compensation constants or ID #1 */
+};
+
+enum bq27000_status_flags {
+	BQ27000_STATUS_CHGS = 0x80, /* 1 = being charged */
+	BQ27000_STATUS_NOACT = 0x40, /* 1 = no activity */
+	BQ27000_STATUS_IMIN = 0x20, /* 1 = Lion taper current mode */
+	BQ27000_STATUS_CI = 0x10, /* 1 = capacity likely  innacurate */
+	BQ27000_STATUS_CALIP = 0x08, /* 1 = calibration in progress */
+	BQ27000_STATUS_VDQ = 0x04, /* 1 = capacity should be accurate */
+	BQ27000_STATUS_EDV1 = 0x02, /* 1 = end of discharge.. <6% left */
+	BQ27000_STATUS_EDVF = 0x01, /* 1 = no, it's really empty now */
+};
+
+#endif
 /* If the system has several batteries we need a different name for each
  * of them...
  */
@@ -99,7 +183,7 @@ static int bq27x00_battery_temperature(struct bq27x00_device_info *di)
 	int ret;
 	int temp = 0;
 
-	ret = bq27x00_read(BQ27x00_REG_TEMP, &temp, 0, di);
+	ret = bq27x00_read(BQ27x00_TEMP_L, &temp, 0, di);
 	if (ret) {
 		dev_err(di->dev, "error reading temperature\n");
 		return ret;
@@ -120,7 +204,7 @@ static int bq27x00_battery_voltage(struct bq27x00_device_info *di)
 	int ret;
 	int volt = 0;
 
-	ret = bq27x00_read(BQ27x00_REG_VOLT, &volt, 0, di);
+	ret = bq27x00_read(BQ27x00_VOLT_L, &volt, 0, di);
 	if (ret) {
 		dev_err(di->dev, "error reading voltage\n");
 		return ret;
@@ -140,7 +224,7 @@ static int bq27x00_battery_current(struct bq27x00_device_info *di)
 	int curr = 0;
 	int flags = 0;
 
-	ret = bq27x00_read(BQ27x00_REG_AI, &curr, 0, di);
+	ret = bq27x00_read(BQ27x00_AI_L, &curr, 0, di);
 	if (ret) {
 		dev_err(di->dev, "error reading current\n");
 		return 0;
@@ -150,12 +234,12 @@ static int bq27x00_battery_current(struct bq27x00_device_info *di)
 		/* bq27500 returns signed value */
 		curr = (int)(s16)curr;
 	} else {
-		ret = bq27x00_read(BQ27x00_REG_FLAGS, &flags, 0, di);
+		ret = bq27x00_read(BQ27x00_FLAGS, &flags, 0, di);
 		if (ret < 0) {
 			dev_err(di->dev, "error reading flags\n");
 			return 0;
 		}
-		if (flags & BQ27000_FLAG_CHGS) {
+		if (flags & BQ27000_STATUS_CHGS) {
 			dev_dbg(di->dev, "negative current!\n");
 			curr = -curr;
 		}
@@ -174,9 +258,9 @@ static int bq27x00_battery_rsoc(struct bq27x00_device_info *di)
 	int rsoc = 0;
 
 	if (di->chip == BQ27500)
-		ret = bq27x00_read(BQ27500_REG_SOC, &rsoc, 0, di);
+		ret = bq27x00_read(BQ27x00_CSOC, &rsoc, 0, di);
 	else
-		ret = bq27x00_read(BQ27000_REG_RSOC, &rsoc, 1, di);
+		ret = bq27x00_read(BQ27x00_RSOC, &rsoc, 1, di);
 	if (ret) {
 		dev_err(di->dev, "error reading relative State-of-Charge\n");
 		return ret;
@@ -192,7 +276,7 @@ static int bq27x00_battery_status(struct bq27x00_device_info *di,
 	int status;
 	int ret;
 
-	ret = bq27x00_read(BQ27x00_REG_FLAGS, &flags, 0, di);
+	ret = bq27x00_read(BQ27x00_FLAGS, &flags, 0, di);
 	if (ret < 0) {
 		dev_err(di->dev, "error reading flags\n");
 		return ret;
@@ -206,7 +290,7 @@ static int bq27x00_battery_status(struct bq27x00_device_info *di,
 		else
 			status = POWER_SUPPLY_STATUS_CHARGING;
 	} else {
-		if (flags & BQ27000_FLAG_CHGS)
+		if (flags & BQ27000_STATUS_CHGS)
 			status = POWER_SUPPLY_STATUS_CHARGING;
 		else
 			status = POWER_SUPPLY_STATUS_DISCHARGING;
@@ -269,14 +353,14 @@ static int bq27x00_battery_get_property(struct power_supply *psy,
 		val->intval = bq27x00_battery_temperature(di);
 		break;
 	case POWER_SUPPLY_PROP_TIME_TO_EMPTY_NOW:
-		ret = bq27x00_battery_time(di, BQ27x00_REG_TTE, val);
+		ret = bq27x00_battery_time(di, BQ27x00_TTE_L, val);
 		break;
 	case POWER_SUPPLY_PROP_TIME_TO_EMPTY_AVG:
-		ret = bq27x00_battery_time(di, BQ27x00_REG_TTECP, val);
+		ret = bq27x00_battery_time(di, BQ27x00_TTECP_L, val);
 		break;
 	case POWER_SUPPLY_PROP_TIME_TO_FULL_NOW:
-		ret = bq27x00_battery_time(di, BQ27x00_REG_TTF, val);
-		break;
+		ret = bq27x00_battery_time(di, BQ27x00_TTF_L, val);
+		break; 
 	default:
 		return -EINVAL;
 	}
