0ef0e43d74413ca6f85434e44539a68ba0086250
Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>
2011-03-25 12:22:31 +0200

    et8ek8: Compile fix for V4L2 control handler registration.
    Signed-off-by: Sakari Ailus <sakari.ailus@maxwell.research.nokia.com>

--- drivers/media/video/et8ek8.c
+++ drivers/media/video/et8ek8.c
@@ -1141,7 +1141,7 @@ static int __exit et8ek8_remove(struct i2c_client *client)
 
 	v4l2_device_unregister_subdev(&sensor->subdev);
 	device_remove_file(&client->dev, &dev_attr_priv_mem);
-	v4l2_ctrl_handler_free(&sensor->ctrls);
+	v4l2_ctrl_handler_free(&sensor->ctrl_handler);
 	media_entity_cleanup(&sensor->subdev.entity);
 	if (sensor->vana)
 		regulator_put(sensor->vana);
