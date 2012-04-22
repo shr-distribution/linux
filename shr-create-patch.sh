9b2e268b469a98c1853e08cc66a01fb901e84733
Martin Jansa <Martin.Jansa@gmail.com>
2012-04-23 00:34:43 +0200

    shr-create-patch.sh: update for 3.2.15
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- shr-create-patch.sh
+++ shr-create-patch.sh
@@ -2,11 +2,11 @@
 
 cat <<EOF > shr.patch
 All patches from shr kernel repository
-rebased on top latest stable patch (linux-stable tag v3.2.12 now)
+rebased on top latest stable patch (linux-stable tag v3.2.15 now)
 
 https://gitorious.org/shr/linux/commits/shr-3.2-nodrm
 
-$(git log --pretty=format:"%h: %s" v3.2.12..)
+$(git log --pretty=format:"%h: %s" v3.2.15..)
 
-$(git diff v3.2.12)
+$(git diff v3.2.15)
 EOF
