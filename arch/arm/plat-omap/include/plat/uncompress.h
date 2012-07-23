f0bd45806c7dafc06455afddf489890e4d181fca
NeilBrown <neilb@suse.de>
2012-07-23 10:42:54 +1000

    GTA04: enable early_printk
    We need to tell the kernel which UART is used for printk.
    Naturally, this happens in "uncompress.h".
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -165,6 +165,7 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 		DEBUG_LL_OMAP3(3, omap3517evm);
 		DEBUG_LL_OMAP3(3, omap3_beagle);
 		DEBUG_LL_OMAP3(3, omap3_pandora);
+		DEBUG_LL_OMAP3(3, gta04);
 		DEBUG_LL_OMAP3(3, omap_ldp);
 		DEBUG_LL_OMAP3(3, overo);
 		DEBUG_LL_OMAP3(3, touchbook);
