eb9019d6556574531ecf6cfa0f830899118b231b
NeilBrown <neilb@suse.de>
2012-08-17 16:50:15 +1000

    GTA04: Enable mdac hwmon
    Useful for testing.
    
      /sys/class/hwmon/hwmon0/device/in7_input
    
    show the reading for MICSENSE which detects a jack plugged in.
    
    You need HSMIC_BIAS enabled which is forced by the next patch
    or can be achieved with alsamixer by
      "Analog Left Headset Mic"  set to capture, and so loopback enabled,
      e.g.
        HeadsetL Mixer AudioL1  not muted
        Left1 Analog Loopback   not muted
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- arch/arm/mach-omap2/board-omap3gta04.c
+++ arch/arm/mach-omap2/board-omap3gta04.c
@@ -1154,6 +1154,12 @@ static struct platform_device gta04_vaux3_virtual_regulator_device = {
 };
 #endif
 
+
+static struct platform_device madc_hwmon = {
+	.name	= "twl4030_madc_hwmon",
+	.id	= -1,
+};
+
 static struct platform_device *gta04_devices[] __initdata = {
 //	&leds_gpio,
 	&keys_gpio,
@@ -1178,6 +1184,7 @@ static struct platform_device *gta04_devices[] __initdata = {
 #if defined(CONFIG_SND_SOC_W2CBW003)
 	&gta04_w2cbw003_codec_audio_device,
 #endif
+	&madc_hwmon,
 };
 
 static const struct usbhs_omap_board_data usbhs_bdata __initconst = {
