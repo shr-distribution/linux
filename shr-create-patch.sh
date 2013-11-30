696bb70fd3b27f542015f28fac7838bf590902c2
Martin Jansa <Martin.Jansa@gmail.com>
2013-11-30 02:51:19 +0100

    shr-create-patch: update stable version
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- shr-create-patch.sh
+++ shr-create-patch.sh
@@ -2,11 +2,11 @@
 
 cat <<EOF > shr.patch
 All patches from shr kernel repository
-rebased on top latest stable patch (linux-stable tag v3.2.15 now)
+rebased on top latest stable patch (linux-stable tag v3.2.52 now)
 
 https://gitorious.org/shr/linux/commits/shr-3.2-nodrm
 
-$(git log --pretty=format:"%h: %s" v3.2.15..)
+$(git log --pretty=format:"%h: %s" v3.2.52..)
 
-$(git diff v3.2.15)
+$(git diff v3.2.52)
 EOF
