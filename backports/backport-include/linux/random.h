ec46651c7f02aa953d75f56ca3be15b148f75f63
Christophe Chapuis <chris.chapuis@gmail.com>
2021-03-28 09:52:33 +0000

    lineageos_hammerhead_defconfig: reuse defconfig from Halium 5.1
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- backports/backport-include/linux/random.h
+++ backports/backport-include/linux/random.h
@@ -12,16 +12,6 @@ static inline void add_device_randomness(const void *buf, unsigned int size)
 }
 #endif
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(3,8,0)
-/* backports 496f2f9 */
-#define prandom_seed(_seed)		srandom32(_seed)
-#define prandom_u32()			random32()
-#define prandom_u32_state(_state)	prandom32(_state)
-/* backport 6582c665d6b882dad8329e05749fbcf119f1ab88 */
-#define prandom_bytes LINUX_BACKPORT(prandom_bytes)
-void prandom_bytes(void *buf, int bytes);
-#endif
-
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,14,0)
 /**
  * prandom_u32_max - returns a pseudo-random number in interval [0, ep_ro)
