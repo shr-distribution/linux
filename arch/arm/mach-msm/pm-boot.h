772228294370696c428383eae99c124e17d3f6f3
Martin Jansa <Martin.Jansa@gmail.com>
2021-05-10 20:10:59 +0200

    mach-msm: use __devinit on msm_pm_boot_init
    * to match with pm-boot.h declaration and fix:
    WARNING: vmlinux.o(.devinit.text+0x2b6c): Section mismatch in reference from the function msm_pm_boot_probe() to the function .init.text:msm_pm_boot_init()
    The function __devinit msm_pm_boot_probe() references
    a function __init msm_pm_boot_init().
    If msm_pm_boot_init is only used by msm_pm_boot_probe then
    annotate msm_pm_boot_init with a matching annotation.
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- arch/arm/mach-msm/pm-boot.h
+++ arch/arm/mach-msm/pm-boot.h
@@ -31,7 +31,7 @@ struct msm_pm_boot_platform_data {
 };
 
 #ifdef CONFIG_PM
-int __init msm_pm_boot_init(struct msm_pm_boot_platform_data *pdata);
+int __devinit msm_pm_boot_init(struct msm_pm_boot_platform_data *pdata);
 #else
 static inline int __init msm_pm_boot_init(
 		struct msm_pm_boot_platform_data *pdata)
