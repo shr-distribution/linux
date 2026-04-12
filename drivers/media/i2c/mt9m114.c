c23bdafd4c9da907f3354d6a8d888159b7d48e5b
Herman van Hazendonk <github.com@herrie.org>
2026-04-12 06:28:33 +0200

    Revert "media: i2c: mt9m114: Fix Cb/Cr order by adding SWAP_RED_BLUE for UYVY"
    This reverts commit 119b5c37c4b974899a77fe191bfec8527029432a.

--- drivers/media/i2c/mt9m114.c
+++ drivers/media/i2c/mt9m114.c
@@ -624,18 +624,11 @@ static const struct mt9m114_format_info mt9m114_format_infos[] = {
 		 */
 		.code = MEDIA_BUS_FMT_UYVY8_2X8,
 		.flags = MT9M114_FMT_FLAG_PARALLEL,
-		/*
-		 * VFE31 DEMUX configured for UYVY produces NV21 instead of NV12,
-		 * indicating sensor actually outputs VYUY by default. Add
-		 * SWAP_RED_BLUE to swap Cb/Cr and get proper UYVY output.
-		 */
-		.output_format = MT9M114_CAM_OUTPUT_FORMAT_FORMAT_YUV
-			       | MT9M114_CAM_OUTPUT_FORMAT_SWAP_RED_BLUE,
+		.output_format = MT9M114_CAM_OUTPUT_FORMAT_FORMAT_YUV,
 	}, {
 		.code = MEDIA_BUS_FMT_UYVY8_1X16,
 		.flags = MT9M114_FMT_FLAG_CSI2,
-		.output_format = MT9M114_CAM_OUTPUT_FORMAT_FORMAT_YUV
-			       | MT9M114_CAM_OUTPUT_FORMAT_SWAP_RED_BLUE,
+		.output_format = MT9M114_CAM_OUTPUT_FORMAT_FORMAT_YUV,
 	}, {
 		.code = MEDIA_BUS_FMT_YUYV8_2X8,
 		.flags = MT9M114_FMT_FLAG_PARALLEL,
